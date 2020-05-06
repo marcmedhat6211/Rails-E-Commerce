1119e809770979a3132dfa1cfe793daacfe34234
ahmedgalaldev <ahmedgalaldev@gmail.com>
2020-05-07 00:13:59 +0200

    use devise gem

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-   require 'carrierwave/orm/activerecord'
+   
 
   def index
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get 'welcome/index'
-  devise_for :users
+  devise_for :users, path: 'user', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
