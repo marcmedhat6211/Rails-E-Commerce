e8c5fbc7df168dbfde362f5a1faf49a46e46fc4a
ahmedgalaldev <ahmedgalaldev@gmail.com>
2020-05-06 22:33:00 +0200

    config devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'welcome/index'
   devise_for :users
+  root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
