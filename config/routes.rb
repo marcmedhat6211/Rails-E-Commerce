1a071b260f044ad8172e0f6294c0cbef3ef9ff60
Mahmoud-ramadan727 <mahmoud.ramdan1994@gmail.com>
2020-05-07 11:04:02 +0200

    optimize devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get 'welcome/index'
-  devise_for :users, path: 'user', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
-  
+  # devise_for :users, path: 'user', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register'  }
+  devise_scope :user do
+    get "/user/sign_in" => "devise/sessions#new" # custom path to login/sign_in
+    get "/user/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+  end
+
+  # Below for all other routes:
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
