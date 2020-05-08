7876c0ee2a9bbd5b5bf3d48663c440a43a711da1
marcmedhat6211 <marcmedhat6211@gmail.com>
2020-05-08 16:49:22 +0200

    CR operations on products, categories & brands works

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,12 +1,20 @@
 class BrandsController < ApplicationController
 
+    def index
+        @brands = Brand.all
+    end
+    
+    def show
+        @brand = Brand.find(params[:id])
+    end
+    
     def new
     end
 
     def create
         @brand = Brand.new(brand_params)
         @brand.save
-        # redirect_to @brand
+        redirect_to @brand
     end
 
     private
