88234db9edf81399831477c8e7baaad7555c77eb
yisaleh <yara_ahmed_saleh@yahoo.com>
2020-05-10 03:07:22 +0200

    search feature

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+
+        
+        def search  
+            if params[:search].blank?  
+              redirect_to(root_path, alert: "Empty field!") and return  
+            else  
+              @parameter = params[:search].downcase  
+              @results = Product.all.where("lower(title) LIKE :search", search: @parameter)  
+            end  
+          end
+      end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'welcome/index'
   devise_for :users
+  get 'pages/search' => 'pages#search', :as => 'search_page'
+  get 'orders/checkout' => 'orders#create'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
