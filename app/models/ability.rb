ba8d46d5ac6781b9fa2e11148c209f6919b1f85b
marcmedhat6211 <marcmedhat6211@gmail.com>
2020-05-12 21:05:27 +0200

    abilities back to normal and home button at navbar

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,7 +7,6 @@ class Ability
     # Define abilities for the passed in user here. For example:
 
       user ||= User.new # guest user (not logged in)
-      can :manage, :all
       if user.admin_role?
           can :manage, :all
           can :manage, Store
