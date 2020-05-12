9915c0ea08cad37a49d0f39d49803b2648de5f2b
marcmedhat6211 <marcmedhat6211@gmail.com>
2020-05-12 13:51:42 +0200

    orders model testing

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -4,4 +4,10 @@ class OrderTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save order without status" do
+    order = Order.new
+    assert_not order.save, "saved the order without a status"
+  end
+
 end
